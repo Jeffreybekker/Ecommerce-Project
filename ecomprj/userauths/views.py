9ca3bc087e3b417794020b1098ddddc0d8953a12
Jeffrey Bekker <jef.bekker@gmail.com>
2024-08-12 18:55:52 +0200

    Small changes

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.conf import settings
+from django.contrib import messages
 
 # Create your views here.
 def register_view(request):
     
-    form = UserRegisterForm()
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST or None)    
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, your account was created succesfully!")
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect("core:index")
+        
+    else:
+        form = UserRegisterForm()
+        
+        
     context = {
         'form': form,
     }
+    
     return render(request, "userauths/sign-up.html", context)
+
