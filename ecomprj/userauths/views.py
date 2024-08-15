19562ba101806b47e6881432518ee518b04ce04f
Jeffrey Bekker <jef.bekker@gmail.com>
2024-08-15 21:39:06 +0200

    Added sign-in function

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import login, authenticate
 from django.conf import settings
 from django.contrib import messages
 
+User = settings.AUTH_USER_MODEL
+
 # Create your views here.
 def register_view(request):
     
@@ -28,3 +30,30 @@ def register_view(request):
     
     return render(request, "userauths/sign-up.html", context)
 
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("core:index")
+    
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} does not exist")
+            
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in.")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "User Does Not Exist. Create an account.")
+
+    context = {
+        
+    }
+    
+    return render(request, "userauths/sign-in.html", context)
