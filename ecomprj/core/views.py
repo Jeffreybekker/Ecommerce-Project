b040c56e1900da84dbb911224477684a45860b59
Jeffrey Bekker <jef.bekker@gmail.com>
2024-09-03 19:29:22 +0200

    A working page to visit the vendors

--- ecomprj/core/views.py
+++ ecomprj/core/views.py
@@ -54,9 +54,11 @@ def vendor_list_view(request):
 
 def vendor_detail_view(request, vid):
     vendor = Vendor.objects.get(vid=vid)
+    products = Product.objects.filter(vendor=vendor, product_status="published")
 
     context = {
         "vendor": vendor,
+        "products": products,
     }
     
     return render(request, "core/vendor-detail-list.html", context)
