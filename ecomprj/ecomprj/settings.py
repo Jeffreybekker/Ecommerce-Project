df3cb84734c6ac36b282f4893abc4d14a5d9a2fa
Jeffrey Bekker <jef.bekker@gmail.com>
2024-09-04 18:45:53 +0200

    Installed pip taggit and add to settings

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # Third party
+    'taggit', 
+    
     # Custom Apps
     'core',
     'userauths',
