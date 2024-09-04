8e5f6a198044f27749ca934be0164092382fca8b
Jeffrey Bekker <jef.bekker@gmail.com>
2024-09-04 12:44:27 +0200

    product details added

--- ecomprj/core/urls.py
+++ ecomprj/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_list_view, vendor_detail_view
+from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_list_view, vendor_detail_view, product_detail_view
 
 app_name = "core"
 
@@ -7,6 +7,7 @@ urlpatterns = [
     # homepage
     path("", index, name="index"),
     path("products/", product_list_view, name="product-list"),
+    path("products/<pid>", product_detail_view, name="product-detail"),
     
     # Category
     path("category/", category_list_view, name="category-list"),
