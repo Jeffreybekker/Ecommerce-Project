1df4bb31e7e265e1428a1e07b967773897e87381
Jeffrey Bekker <jef.bekker@gmail.com>
2024-09-12 14:00:00 +0200

    Added functionality for the filter range slider

--- ecomprj/core/context_processor.py
+++ ecomprj/core/context_processor.py
@@ -1,8 +1,11 @@
 from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Wishlist, Address
+from django.db.models import Min, Max
 
 def default(request):
     categories = Category.objects.all()
     vendors = Vendor.objects.all()
+    
+    min_max_price = Product.objects.aggregate(Min("price"), Max("price")) # Get the lowest and highest value from the models (price from product)
     
     try:
         address = Address.objects.get(user=request.user)
@@ -13,4 +16,5 @@ def default(request):
         "categories": categories,
         "address": address,
         "vendors": vendors,
+        "min_max_price": min_max_price,
     }
